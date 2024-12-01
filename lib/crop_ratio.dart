3a3d0b40994315db4752e029f2b018f706ea1b80
Mehdi Mousavi <malemir@gmail.com>
2024-12-01 16:54:16 +0330

    Changes on crop ratios and default option

--- lib/crop_ratio.dart
+++ lib/crop_ratio.dart
@@ -3,12 +3,11 @@ library pictus;
 import 'package:flutter/material.dart';
 
 enum CropRatio {
+  free(title: 'Custom', ratio: null, icon: Icons.crop_free),
   r1_1(title: '1 : 1', ratio: 1 / 1, icon: Icons.crop_din),
   r16_9(title: '16 : 9', ratio: 16 / 9, icon: Icons.crop_16_9),
   r3_2(title: '3 : 2', ratio: 3 / 2, icon: Icons.crop_3_2),
-  r5_4(title: '5 : 4', ratio: 5 / 4, icon: Icons.crop_5_4),
-  r7_5(title: '7 : 5', ratio: 7 / 5, icon: Icons.crop_7_5),
-  free(title: 'custom', ratio: null, icon: Icons.crop_free);
+  r4_3(title: '4 : 3', ratio: 4 / 3, icon: Icons.crop_5_4);
 
   final String title;
   final double? ratio;
